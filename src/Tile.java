bfcb75d738d76945aee6d3a4a49cf853a7761eea
Alejandro Zamora <azamora8@depaul.edu>
2021-10-17 19:44:38 -0500

    Adding additional fields to each Tile
    Modified Tile fields on the json file to track additional fields the Sweeper should be looking out for . Tile.java has been modified to be able to read the contents of the Tiles.

--- src/Tile.java
+++ src/Tile.java
@@ -1,8 +1,12 @@
 package src;
 
 
+import com.google.gson.JsonArray;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
+
+import java.lang.reflect.Array;
+import java.util.List;
 
 
 public class Tile {
@@ -22,45 +26,111 @@ private String downTile;
 @SerializedName("leftTile")
 @Expose
 private String leftTile;
+@SerializedName("obstaclePresent")
+@Expose
+private String obstaclePresent;
+@SerializedName("doorPresent")
+@Expose
+private String doorPresent;
+@SerializedName("stairsEdge")
+@Expose
+private String stairsEdge;
+@SerializedName("chargingStation")
+@Expose
+private String chargingStation;
+@SerializedName("floorType")
+@Expose
+private String floorType;
+@SerializedName("dirt")
+@Expose
+private String dirt;
 
 public String getTile() {
-return tile;
-}
+       return tile;
+   }
 
 public void setTile(String tile) {
-this.tile = tile;
-}
+        this.tile = tile;
+   }
 
 public String getUpTile() {
-return upTile;
-}
+        return upTile;
+    }
 
 public void setUpTile(String upTile) {
-this.upTile = upTile;
-}
+        this.upTile = upTile;
+    }
 
 public String getRightTile() {
-return rightTile;
-}
+        return rightTile;
+    }
 
 public void setRightTile(String rightTile) {
-this.rightTile = rightTile;
-}
+        this.rightTile = rightTile;
+    }
 
 public String getDownTile() {
-return downTile;
-}
+        return downTile;
+    }
 
 public void setDownTile(String downTile) {
-this.downTile = downTile;
-}
+        this.downTile = downTile;
+    }
 
 public String getLeftTile() {
-return leftTile;
-}
+        return leftTile;
+    }
 
 public void setLeftTile(String leftTile) {
-this.leftTile = leftTile;
-}
+        this.leftTile = leftTile;
+   }
+
+public String getObstaclePresent() {
+        return obstaclePresent;
+    }
+
+public void setObstaclePresent(String obstaclePresent) {
+        this.obstaclePresent = obstaclePresent;
+    }
+
+public String getDoorPresent() {
+        return doorPresent;
+    }
+
+public void setDoorPresent(String doorPresent) {
+        this.doorPresent = doorPresent;
+    }
+
+public String getStairsEdge() {
+        return stairsEdge;
+    }
+
+public void setStairsEdge(String stairsEdge) {
+        this.stairsEdge = stairsEdge;
+    }
+
+public String getChargingStation() {
+        return chargingStation;
+    }
+
+public void setChargingStation(String chargingStation) {
+        this.chargingStation = chargingStation;
+    }
+
+public String getFloorType() {
+        return floorType;
+    }
+
+public void setFloorType(String floorType) {
+        this.floorType = floorType;
+    }
+
+public String getDirt() {
+        return dirt;
+    }
+
+public void setDirt(String dirt) {
+        this.dirt = dirt;
+    }
 
 }
